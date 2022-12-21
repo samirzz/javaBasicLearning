4ccab5907ef12a301d62644447cbfa51e5fbae34
Samirzz <bhandarisamir73@gmail.com>
2022-12-21 08:56:56 +0545

    use of getter and setter for encapsulation

---
+++ getterSetter/GetterSetterExample.java
@@ -0,0 +1,22 @@
+package oop.encapsulation.getterSetter;
+
+public class GetterSetterExample {
+    private int num;
+    private String name;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ getterSetter/Main.java
@@ -0,0 +1,12 @@
+package oop.encapsulation.getterSetter;
+
+public class Main {
+    public static void main(String[] args) {
+        GetterSetterExample gse= new GetterSetterExample();
+        gse.setName("samir");
+        gse.setNum(1);
+        System.out.println("id = "+gse.getNum());
+        System.out.println("name = "+gse.getName() +" "+ "bhandari");
+
+    }
+}
